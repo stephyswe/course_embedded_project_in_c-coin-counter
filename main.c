30e2041778a97823a6624388a884db2b67b3b8bf
Stephanie <stephaniiehallberg@gmail.com>
2023-10-23 23:31:21 +0200

    feat: coin category counter - appear 2s after button click * use millis to enable button click while waiting

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include "system.h"
 
 // Function to handle button click
-void HandleButtonClick(int value, int *coinCount, int *coinCounter) {
+void HandleButtonClick(int value, int *coinCount, int *coinCounter, int *randomMessageFlag, uint32_t *lastCoinTime) {
     _delay_ms(200);
     lcd_clear();
     lcd_set_cursor(0, 1);
@@ -26,6 +26,30 @@ void HandleButtonClick(int value, int *coinCount, int *coinCounter) {
     lcd_printf("%d Coin(s)", *coinCount);
     lcd_set_cursor(0, 1);
     lcd_printf("Total: %d SEK", *coinCounter);
+
+    // Set the last coin press time to the current time
+    *lastCoinTime = millis();
+
+    // Reset the "Random" message flag
+    *randomMessageFlag = 0;
+}
+
+// Function to display the coin counts
+void DisplayCoinCounts(int coinCount, int coinValues[NUM_BUTTONS]) {
+    lcd_clear();
+    lcd_set_cursor(0, 0);
+
+    // Create a message like "A: x / B: y" where x and y are the counts of coins A and B
+    for (int i = 0; i < NUM_BUTTONS; i++) {
+        lcd_printf("%c: %d", 'A' + i, coinCount / coinValues[i]);
+
+        if (i == 1) {
+            // Move to the next line after displaying A and B counts
+            lcd_set_cursor(0, 1);
+        } else if (i < NUM_BUTTONS - 1) {
+            lcd_printf(" / ");
+        }
+    }
 }
 
 // Initialize hardware and peripherals
@@ -46,12 +70,33 @@ void MainLoop() {
     int coinCount = 0;
     int systemOn = 1;
     int coinValues[NUM_BUTTONS] = {1, 5, 10, 25};
+    int buttonFlag = 1; // flag on button click
+    uint32_t lastCoinTime = millis(); // Track time of last coin press
+    
+    // Initialize last clicked coin value
+    int lastClickedCoin = -1;
+    int categoryCounts[NUM_BUTTONS] = {0}; // Initialize to all zeros
 
     while (1) {
         if (systemOn) {
             for (int i = 0; i < NUM_BUTTONS; i++) {
                 if (bit_is_clear(PINB, BUTTON_PIN_1 + i)) {
-                    HandleButtonClick(coinValues[i], &coinCount, &coinCounter);
+                    HandleButtonClick(coinValues[i], &coinCount, &coinCounter, &buttonFlag, &lastCoinTime);
+                     lastClickedCoin = coinValues[i];
+
+                     // category index for clicked coin
+                    int categoryIndex = -1;
+                    for (int j = 0; j < NUM_BUTTONS; j++) {
+                        if (lastClickedCoin == coinValues[j]) {
+                            categoryIndex = j;
+                            break;
+                        }
+                    }
+
+                    // Update categoryCounts array
+                    if (categoryIndex != -1) {
+                        categoryCounts[categoryIndex] += 1;
+                    }
                 }
             }
         }
@@ -66,6 +111,29 @@ void MainLoop() {
         }
 
         lcd_set_cursor(0, 1);
+
+        // Check if it's time to display "Random"
+        if (!buttonFlag) {
+            uint32_t currentTime = millis();
+            if (currentTime - lastCoinTime >= 4000) { // 4 seconds have passed
+                lcd_clear();
+                lcd_set_cursor(0, 0);
+
+
+                if (lastClickedCoin != -1) {
+                    lcd_clear();
+
+                    lcd_printf("A: %d / B: %d", categoryCounts[0], categoryCounts[1]);
+                    lcd_set_cursor(0, 1);
+                    lcd_printf("C: %d / D: %d", categoryCounts[2], categoryCounts[3]);
+                }
+
+                 else {
+                    lcd_printf("User None"); // No coin clicked
+                }
+                buttonFlag = 1; // Set the "Random" message flag
+            }
+        }
     }
 }
 
